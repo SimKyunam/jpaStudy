42c0edaad2b29aa5a272e09332ef9a4dd658685c
kyunam Sim <ppp9026@naver.com>
2021-09-10 20:04:11 +0900

    프로시저 사용

--- src/main/java/com/jpa/demo/querydsl/domain/Hello.java
+++ src/main/java/com/jpa/demo/querydsl/domain/Hello.java
@@ -5,15 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@NamedStoredProcedureQuery(
+        name = "multiply",
+        procedureName = "proc_multiply",
+        parameters = {
+                @StoredProcedureParameter(name = "inParam", mode =
+                        ParameterMode.IN, type = Integer.class),
+                @StoredProcedureParameter(name = "outParam", mode =
+                        ParameterMode.OUT, type = Integer.class)
+        }
+)
 public class Hello {
     @Id
     @GeneratedValue
--- src/test/java/com/jpa/demo/jpaorm/procedure/ProcedureTest.java
+++ src/test/java/com/jpa/demo/jpaorm/procedure/ProcedureTest.java
@@ -4,13 +4,61 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ProcedureTest {
-    @DisplayName("1. ")
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @DisplayName("1. 순서기반 프로시져")
     @Test
     void test_1(){
+        StoredProcedureQuery spq =
+                em.createStoredProcedureQuery("proc_multiply"); //Hello
+        spq.registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN);
+        spq.registerStoredProcedureParameter(2, Integer.class, ParameterMode.OUT);
 
+        spq.setParameter(1, 100);
+        spq.execute();
+
+        Integer out = (Integer) spq.getOutputParameterValue(2);
+        System.out.println("out = " + out);
     }
+
+    @DisplayName("2. 파라미터 이름 사용")
+    @Test
+    void test_2(){
+        StoredProcedureQuery spq =
+                em.createStoredProcedureQuery("proc_multiply"); //Hello
+        spq.registerStoredProcedureParameter("inParam", Integer.class, ParameterMode.IN);
+        spq.registerStoredProcedureParameter("outParam", Integer.class, ParameterMode.OUT);
+
+        spq.setParameter(1, 100);
+        spq.execute();
+
+        Integer out = (Integer) spq.getOutputParameterValue("outParam");
+        System.out.println("out = " + out);
+    }
+
+    @DisplayName("3. xml 프로시저 사용(procedure.xml)")
+    @Test
+    void test_3(){
+        StoredProcedureQuery spq =
+//                em.createStoredProcedureQuery("multiplyXml");
+                em.createStoredProcedureQuery("multiply");
+        spq.setParameter("inParam", 100);
+        spq.execute();
+
+        Integer out = (Integer) spq.getOutputParameterValue("outParam");
+        System.out.println("out = " + out);
+    }
+
+
 }
